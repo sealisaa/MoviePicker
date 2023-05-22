83c017daa347c3bc4c7282e625a65d0629ae3dbc
osokindm <osokin34@gmail.com>
2023-05-22 14:25:07 +0300

    search is now working

---
+++ app/src/main/java/com/example/moviepicker/ui/SearchFiltersResultFragment.kt
@@ -0,0 +1,156 @@
+package com.example.moviepicker.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import api.service.KPApiService
+import com.example.moviepicker.MoviesByFiltersPagedListRepository
+import com.example.moviepicker.adapters.MoviesByFiltersAdapter
+import com.example.moviepicker.data.api.DBClient
+import com.example.moviepicker.data.repository.NetworkState
+import com.example.moviepicker.databinding.FragmentSearchFiltersResultBinding
+import com.example.moviepicker.ui.viewmodel.MoviesByGenreViewModel
+
+
+class SearchFiltersResultFragment : Fragment() {
+
+    private var _binding: FragmentSearchFiltersResultBinding ? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: MoviesByGenreViewModel
+
+    lateinit var movieRepository: MoviesByFiltersPagedListRepository
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchFiltersResultBinding.inflate(inflater)
+        val apiService: KPApiService = DBClient.getClient()
+        movieRepository = MoviesByFiltersPagedListRepository(apiService)
+        val keyword = arguments?.getString("keyword")
+        val rating = arguments?.getInt("rating")
+        val yearFrom = arguments?.getInt("yearFrom")
+        val yearTo = arguments?.getInt("yearTo")
+        val countryName = arguments?.getString("countryName")
+        val genreName = arguments?.getString("genreName")
+
+        viewModel =
+            getViewModel(
+                keyword = keyword,
+                rating = rating,
+                yearFrom = yearFrom,
+                yearTo = yearTo,
+                countryId = getCountryIdByTitle(countryName),
+                genreId = getGenreIdByTitle(genreName)
+            )
+        Log.d("data", "keyword " + (keyword ?: "NULL"))
+        Log.d("data", "rating " + rating.toString())
+        Log.d("data", "yearFrom " + yearFrom.toString())
+        Log.d("data", "yearTo " + yearTo.toString())
+        Log.d("data", "countryId " + getCountryIdByTitle(countryName).toString())
+        Log.d("data", "genreId " + getGenreIdByTitle(genreName).toString())
+
+        val adapter = MoviesByFiltersAdapter(this)
+        val gridLayoutManager = GridLayoutManager(this.context, 1)
+
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                val viewType = adapter.getItemViewType(position)
+                if (viewType == adapter.DATA_VIEW_TYPE) return 1    // DATA_VIEW_TYPE will occupy 1 out of 3 span
+                else return 3                                            // FOOTER_VIEW_TYPE will occupy all 3 span
+            }
+
+        }
+        binding.apply {
+            moviesRecyclerView.layoutManager = gridLayoutManager
+            moviesRecyclerView.setHasFixedSize(true)
+            moviesRecyclerView.adapter = adapter
+        }
+
+        viewModel.moviePagedList.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
+
+        viewModel.networkState.observe(viewLifecycleOwner) {
+            binding.progressBar.visibility =
+                if (viewModel.listIsEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            binding.textViewConnection.visibility =
+                if (viewModel.listIsEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
+            if (!viewModel.listIsEmpty()) {
+                adapter.setNetworkState(it)
+            }
+        }
+        return binding.root
+    }
+
+    private fun getViewModel(
+        countryId: Int? = null,
+        genreId: Int? = null,
+        rating: Int? = null,
+        yearFrom: Int? = null,
+        yearTo: Int? = null,
+        keyword: String? = null
+    ): MoviesByGenreViewModel {
+        return ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return MoviesByGenreViewModel(
+                    movieRepository, keyword = keyword,
+                    rating = rating,
+                    yearFrom = yearFrom,
+                    yearTo = yearTo,
+                    countryId = countryId,
+                    genreId = genreId
+                ) as T
+            }
+        })[MoviesByGenreViewModel::class.java]
+    }
+
+    private fun getGenreIdByTitle(title: String?): Int? {
+        return when (title) {
+            "Thrillers", "Триллеры" -> 1
+            "Comedies", "Комедии" -> 13
+            "Cartoons", "Мультфильмы" -> 18
+            "Horrors", "Ужасы" -> 17
+            else -> null
+        }
+    }
+
+    private fun getCountryIdByTitle(title: String?): Int? {
+        return when (title) {
+            "Russia", "Россия" -> 34
+            "USSR", "СССР" -> 33
+            "USA", "США" -> 1
+            "Italy", "Италия" -> 10
+            "France", "Франция" -> 3
+            "Germany", "Германия" -> 9
+            else -> null
+        }
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SearchFiltersResultFragment()
+    }
+}
--- app/src/main/java/com/example/moviepicker/ui/SearchResultFragment.kt
+++ app/src/main/java/com/example/moviepicker/ui/SearchResultFragment.kt
@@ -1,73 +1,53 @@
 package com.example.moviepicker.ui
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import api.service.KPApiService
-import com.example.moviepicker.MoviesByFiltersPagedListRepository
-import com.example.moviepicker.adapters.MoviesByFiltersAdapter
-import com.example.moviepicker.data.MoviesByGenreViewModel
+import com.example.moviepicker.adapters.MoviesBySearchAdapter
 import com.example.moviepicker.data.api.DBClient
 import com.example.moviepicker.data.repository.NetworkState
+import com.example.moviepicker.data.repository.SearchPagedListRepository
 import com.example.moviepicker.databinding.FragmentSearchResultBinding
-
+import com.example.moviepicker.ui.viewmodel.SearchResultViewModel
 
 class SearchResultFragment : Fragment() {
 
     private var _binding: FragmentSearchResultBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var viewModel: MoviesByGenreViewModel
+    private lateinit var viewModel: SearchResultViewModel
 
-    lateinit var movieRepository: MoviesByFiltersPagedListRepository
+    lateinit var movieRepository: SearchPagedListRepository
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         _binding = FragmentSearchResultBinding.inflate(inflater)
         val apiService: KPApiService = DBClient.getClient()
-        movieRepository = MoviesByFiltersPagedListRepository(apiService)
-        val keyword = arguments?.getString("keyword")
-        val rating = arguments?.getInt("rating")
-        val yearFrom = arguments?.getInt("yearFrom")
-        val yearTo = arguments?.getInt("yearTo")
-        val countryName = arguments?.getString("countryName")
-        val genreName = arguments?.getString("genreName")
+        movieRepository = SearchPagedListRepository(apiService)
 
-        viewModel =
-            getViewModel(
-                keyword = keyword,
-                rating = rating,
-                yearFrom = yearFrom,
-                yearTo = yearTo,
-                countryId = getCountryIdByTitle(countryName),
-                genreId = getGenreIdByTitle(genreName)
-            )
-        Log.e("data", "keyword " + (keyword ?: "NULL"))
-        Log.e("data", "rating " + rating.toString())
-        Log.e("data", "yearFrom " + yearFrom.toString())
-        Log.e("data", "yearTo " + yearTo.toString())
-        Log.e("data", "countryId " + getCountryIdByTitle(countryName).toString())
-        Log.e("data", "genreId " + getGenreIdByTitle(genreName).toString())
+        val keyword = arguments?.getString("keyword") ?: ""
 
-        val adapter = MoviesByFiltersAdapter(this)
-        val gridLayoutManager = GridLayoutManager(this.context, 1)
+        viewModel = getViewModel(keyword)
 
+        val adapter = MoviesBySearchAdapter(this)
+        var gridLayoutManager = GridLayoutManager(this.context, 1)
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            gridLayoutManager = GridLayoutManager(this.context, 1)
+        }
         gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
             override fun getSpanSize(position: Int): Int {
                 val viewType = adapter.getItemViewType(position)
                 if (viewType == adapter.DATA_VIEW_TYPE) return 1    // DATA_VIEW_TYPE will occupy 1 out of 3 span
                 else return 3                                            // FOOTER_VIEW_TYPE will occupy all 3 span
             }
-
         }
         binding.apply {
             moviesRecyclerView.layoutManager = gridLayoutManager
@@ -91,50 +71,13 @@ class SearchResultFragment : Fragment() {
         return binding.root
     }
 
-    private fun getViewModel(
-        countryId: Int? = null,
-        genreId: Int? = null,
-        rating: Int? = null,
-        yearFrom: Int? = null,
-        yearTo: Int? = null,
-        keyword: String? = null
-    ): MoviesByGenreViewModel {
+    private fun getViewModel(keyword: String): SearchResultViewModel {
         return ViewModelProvider(this, object : ViewModelProvider.Factory {
             override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return MoviesByGenreViewModel(
-                    movieRepository, keyword = keyword,
-                    rating = rating,
-                    yearFrom = yearFrom,
-                    yearTo = yearTo,
-                    countryId = countryId,
-                    genreId = genreId
-                ) as T
+                return SearchResultViewModel(movieRepository, keyword) as T
             }
-        })[MoviesByGenreViewModel::class.java]
+        })[SearchResultViewModel::class.java]
     }
-
-    private fun getGenreIdByTitle(title: String?): Int? {
-        return when (title) {
-            "Thrillers", "Триллеры" -> 1
-            "Comedies", "Комедии" -> 13
-            "Cartoons", "Мультфильмы" -> 18
-            "Horrors", "Ужасы" -> 17
-            else -> null
-        }
-    }
-
-    private fun getCountryIdByTitle(title: String?): Int? {
-        return when (title) {
-            "Russia", "Россия" -> 34
-            "USSR", "СССР" -> 33
-            "USA", "США" -> 1
-            "Italy", "Италия" -> 10
-            "France", "Франция" -> 3
-            "Germany", "Германия" -> 9
-            else -> null
-        }
-    }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -148,9 +91,4 @@ class SearchResultFragment : Fragment() {
         _binding = null
     }
 
-
-    companion object {
-        @JvmStatic
-        fun newInstance() = SearchResultFragment()
-    }
 }
